f7ee63a966ab3ff2259bba770053540ff0ced238
Emily Lewis <31196780+EmilyJoLewis@users.noreply.github.com>
2019-05-26 21:24:16 -0400

    Update main.js

--- basicSingleWebpageTemplates/js/main.js
+++ basicSingleWebpageTemplates/js/main.js
@@ -1,5 +1,6 @@
 //This is a main.js file
 "use strict"
+//this code is place holder code that creates an event listener for the button which adds the current date and time to the page
 let a = document.getElementById("btn").addEventListener("click", function(){
 	let getDate = new Date();
 	getDate.getDate();
